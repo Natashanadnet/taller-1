f8efbde9acd87837101a92b6c87ec12c43d108eb
Natasha Netepczuk <natashanadnet@hotmail.com>
2023-10-04 19:27:01 -0300

    continuacion clase 4 - 5

--- src/main/java/py/edu/ucom/natasha/controllers/PrestamosResource.java
+++ src/main/java/py/edu/ucom/natasha/controllers/PrestamosResource.java
@@ -9,7 +9,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import py.edu.ucom.natasha.entities.Libros;
-import py.edu.ucom.natasha.entities.Usuarios;
 import py.edu.ucom.natasha.servicios.PrestamoService;
 
 @Path("prestamos")
